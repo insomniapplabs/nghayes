19d89a85059da29b41699130e25e93afe1e2cd8c
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-02-02 13:30:40 -0600

    first commit

---
+++ client/app/home/home.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('hayesApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'app/home/home.html',
+        controller: 'HomeCtrl'
+      });
+  });
