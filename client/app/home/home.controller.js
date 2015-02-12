d85b0ab27f113bd01cf063f36debb658cdf0b217
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-02-12 14:16:00 -0600

    add image

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -3,4 +3,10 @@
 angular.module('hayesApp')
   .controller('HomeCtrl', function () {
     
+  })
+  .controller('CarouselCtrl', function($scope) {
+  	$scope.myInterval = 5000;
+  	$scope.slides = [
+  		{ image: '/assets/images/physiciansRegional.jpg' }
+  	];
   });
